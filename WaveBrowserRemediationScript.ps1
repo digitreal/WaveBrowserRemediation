ba1cc4f4e6226d764eaf22b12f9cde4c7726a3a4
digitreal <92342408+digitreal@users.noreply.github.com>
2021-10-12 12:26:46 -0600

    Update WaveBrowserRemediationScript.ps1

--- WaveBrowserRemediationScript.ps1
+++ WaveBrowserRemediationScript.ps1
@@ -5,14 +5,13 @@ Function intro {
     
             .DESCRIPTION
                 The script will first scan and log WaveBrowser artifacts.
-                Prompt user to then stop browser session, remove files, scheduled tasks and registry keys associated with WebBrowser.
+                Stops browser session, remove files, scheduled tasks and registry keys associated with WebBrowser.
             .EXAMPLE
-                Run the script to scan and choose to remove the artifacts.
+                Run the script to scan and remove the artifacts.
     
                 Description
                 -----------
                 Scans for WaveBrowser artifacts.
-                Prompts user to remove items found in scan.
                 Kills any browser sessions.
                 Removes registry keys associated with Wave Browser Hijacking Software.
                 Removes files associated with Wave Browser Hijacking Software.
@@ -21,94 +20,6 @@ Function intro {
         #>
 
     }
-Function CheckBrowserProcesses {
-<#--- Checks and PRINTS OUT all running browser processes ---#>
-
-    Get-Process chrome -ErrorAction SilentlyContinue | Out-File -filePath $filePath -Append
-    Get-Process firefox -ErrorAction SilentlyContinue | Out-File -filePath $filePath -Append
-    Get-Process iexplore -ErrorAction SilentlyContinue | Out-File -filePath $filePath -Append
-    Get-Process msedge -ErrorAction SilentlyContinue | Out-File -filePath $filePath -Append
-    Get-Process SWUpdater -ErrorAction SilentlyContinue | Out-File -filePath $filePath -Append
-	Get-Process wavebrowser -ErrorAction SilentlyContinue | Out-File -filePath $filePath -Append
-}
-
-Function CheckWavesorFS {
-<#--- Checks and PRINTS OUT all files assigned to $dir ---#>
-
-	$dir = "C:\users\*\Wavesor Software",
-	"C:\users\*\WebNavigatorBrowser",
-	"C:\users\*\appdata\local\WaveBrowser",
-	"C:\users\*\appdata\local\WebNavigatorBrowser",
-	"C:\users\*\downloads\Wave Browser*.exe",
-	"C:\users\*\appdata\Roaming\Microsoft\Internet Explorer\Quick Launch\WaveBrowser.lnk",
-	"C:\users\*\appdata\Roaming\Microsoft\Windows\Start Menu\Programs\WaveBrowser.lnk",
-	"C:\ProgramData\Intel\ShaderCache\wavebrowser*",
-	"C:\Users\All Users\Intel\ShaderCache\wavebrowser*",
-	"C:\Windows\Prefetch\WAVEBROWSER*.*"
-	
-	foreach ($path in $dir)
-	{    
-    if(($item = Get-Item -Path $path -ErrorAction SilentlyContinue)) {
-    $item,$path,"Path exists" | Out-File -filePath $filePath -Append
-} else {
-    $item,$path,"Path does not exist`n" | Out-File -filePath $filePath -Append
-	   }
-	}
-}
-
-Function CheckScheduledTasks {
-<#--- Checks and PRINTS OUT all scheduled tasks with the name of *wave* ---#>
-    
-    $tasks = Get-ScheduledTask -TaskName *Wave* | Select-Object -ExpandProperty TaskName
-    foreach ($i in $tasks) {
-       $i,"Scheduled Task Exists`n" | Out-File -filePath $filePath -Append
-    }
-}
-
-Function CheckRegistryKey {
-<#--- Checks and PRINTS OUT all registry keys assigned to $dir under the current user account ---#>
-    
-		$dir = "HKCU:\Software\WaveBrowser",
-		"HKCU:\Software\Wavesor",
-		"HKCU:\Software\WebNavigatorBrowser",
-		"HKCU\Software\Microsoft\Windows\CurrentVersion\Run.Wavesor SWUpdater"
-		
-	foreach ($path in $dir)
-	{    
-    if(($item = Get-Item -Path $path -ErrorAction SilentlyContinue)) {
-    $item,$path,"Path exists" | Out-File -filePath $filePath -Append
-} else {
-    $item,$path,"Path does not exist`n" | Out-File -filePath $filePath -Append
-	   }
-	}
-}
-	
-Function CheckUsersRegistryKey {
-<#--- Iterates and loads all user accounts' hkey_users and checks and PRINTS OUT all registry keys assigned to $dir ---#>
-	
-	$users = (Get-ChildItem -path c:\users).name
-	foreach($user in $users)
-	{
-	reg load "hku\$user" "C:\Users\$user\NTUSER.DAT"
-
-	$dir = "key_users\$user\Software\Clients\StartMenuInternet\wave*.*",
-			"key_users\$user\Software\Microsoft\Windows\CurrentVersion\App Paths\wavebrowser.exe",
-			"key_users\$user\Software\Wavesor",
-			"key_users\$user\Software\Wavesor\SWUpdater",
-			"key_users\$user\Software\Microsoft\Windows\CurrentVersion\run\Wavesor*.*"
-			
-		foreach ($path in $dir)
-		{    
-		if(($item = Get-Item -Path $path -ErrorAction SilentlyContinue)) {
-		$item,$path,"Path exists" | Out-File -filePath $filePath -Append
-	} else {
-		$item,$path,"Path does not exist`n" | Out-File -filePath $filePath -Append
-		   }
-		}
-	reg unload "hku\$user"
-
-	}
-}
 
 Function BrowserProcesses {
 <#--- Checks and STOPS all running browser processes ---#>
@@ -174,66 +85,13 @@ Function RemoveRegistryKey {
 	}
 }	
 
-Function RemoveUsersRegistryKey {
-<#--- Iterates and loads all user accounts' hkey_users and checks and DELETES all registry keys assigned to $dir ---#>
-	
-	$users = (Get-ChildItem -path c:\users).name
-	foreach($user in $users)
-	{
-	reg load "hku\$user" "C:\Users\$user\NTUSER.DAT"
-
-	$dir = "key_users\$user\Software\Clients\StartMenuInternet\wave*.*",
-			"key_users\$user\Software\Microsoft\Windows\CurrentVersion\App Paths\wavebrowser.exe",
-			"key_users\$user\Software\Wavesor",
-			"key_users\$user\Software\Wavesor\SWUpdater",
-			"key_users\$user\Software\Microsoft\Windows\CurrentVersion\run\Wavesor*.*"
-			
-		foreach ($path in $dir)
-		{    
-		if(($item = Get-Item -Path $path -ErrorAction SilentlyContinue)) {
-		$item,$path,"Attempting removal" | Out-File -filePath $filePath -Append
-		Remove-Item -Path $path -Force -ErrorAction SilentlyContinue
-	} else {
-		$item,$path,"Path does not exist`n" | Out-File -filePath $filePath -Append
-		   }
-		}
-	reg unload "hku\$user"
-
-	}
-}
 
 <#------------------------------------------------------------------------#>
 
-$filePath = "C:\temp\waveScan.txt"
-$titleScan = 'Would you like to scan for the Wave Browser on this host'
-$waveScan = '(y/n)?'
-$choicesScan = '&Yes', '&No'
+$filePath = "C:\waveScan.txt"
 
-$decision = $Host.UI.PromptForChoice($titleScan, $waveScan, $choicesScan, 1)
-if ($decision -eq 0) {
-    Write-Host 'confirmed'
-CheckBrowserProcesses
-CheckWavesorFS
-CheckScheduledTasks
-CheckRegistryKey
-CheckUsersRegistryKey
 
-Write-Output "`nPrinting log file to:",$filePath
-
-$titleClean    = 'Would you like to remediate the Wave Browser on this host'
-$waveClean = '(y/n)?'
-$choicesClean  = '&Yes', '&No'
-
-$decision = $Host.UI.PromptForChoice($titleClean, $waveClean, $choicesClean, 1)
-	if ($decision -eq 0) {
-		Write-Host 'confirmed'
-		BrowserProcesses
-		RemoveWavesorFS
-		RemoveScheduledTasks
-		RemoveRegistryKey
-		} else {
-		Write-Host 'cancelled'
-	}
-} else {
-    Write-Host 'cancelled'
-}
+BrowserProcesses
+RemoveWavesorFS
+RemoveScheduledTasks
+RemoveRegistryKey
